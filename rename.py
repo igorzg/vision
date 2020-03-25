1f21e66076535110f7a2044a5f32cb8b7f1e8405
Igor Ivanovic <igor.ivanovic@pm.me>
2020-03-26 00:26:41 +0100

    Initial commit

---
+++ rename.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+# f_path = "./dataset"
+# data = enumerate(os.listdir(f_path))
+# for idx, val in data:
+# 	old_file = os.path.join(f_path, val)
+# 	new_file = os.path.join(f_path, str(idx) + "-" + str(time.time_ns()) + ".jpg")
+# 	print(idx, val)
+# 	os.rename(old_file, new_file)
+
